3242fb7ad7ad5752688a9406a8ffa1abd6b25313
isaacsanft <itsanft@gmail.com>
2026-04-20 18:24:17 -0600

    Mad density and Max coverage unferfit algorithms implemented.

--- core/Algorithms.py
+++ core/Algorithms.py
@@ -8,8 +8,13 @@ class Algorithm:
         self.current_coverage = 0
         self.best_solution = []
         self.best_coverage = 0
+        self.feasible_count = 0
+        self.current_density = 0
+        self.best_density = 0
+        self.min_row_length = 0
 
     def cover(self, column): #Cover function. This is modifies our matrix, "covering" the rows and columns by disconnecting their neighbors from them (but not disconnect them from their neighbors -- this is important)
+        self.feasible_count -= 1
         column.right.left = column.left
         column.left.right = column.right
         col_pointer = column.down
@@ -19,6 +24,8 @@ class Algorithm:
                 row_pointer.up.down = row_pointer.down
                 row_pointer.down.up = row_pointer.up
                 row_pointer.column.size -= 1
+                if row_pointer.column.size == 0:
+                    self.feasible_count -= 1
                 row_pointer = row_pointer.right
             col_pointer = col_pointer.down
 
@@ -29,16 +36,19 @@ class Algorithm:
             while row_pointer != col_pointer:
                 row_pointer.up.down = row_pointer
                 row_pointer.down.up = row_pointer
+                if row_pointer.column.size == 0:
+                    self.feasible_count += 1
                 row_pointer.column.size += 1
                 row_pointer = row_pointer.left
             col_pointer = col_pointer.up
         column.right.left = column
         column.left.right = column
+        self.feasible_count += 1
 
-    # Algorithm X
+    ### Algorithm X - Exact Cover ###
 
     def choose_column(self): #Deterministically choose column with the smallest size.
-        count = 729
+        count = 1000
         position = None
         pointer = self.header.right
         while pointer != self.header:
@@ -70,3 +80,81 @@ class Algorithm:
                 row_reverse_pointer = row_reverse_pointer.left
         self.uncover(column)
         return False
+
+    ### Max Coverage Underfit Algorithm ###
+
+    def length_row(self, row_start):
+        count = 1
+        curr = row_start.right
+        while curr != row_start:
+            count += 1
+            curr = curr.right
+        return count
+
+    def search_uf_cover(self): # This algorithms finds the underfit (no overlap) that covers the most area
+        if self.feasible_count + self.current_coverage <= self.best_coverage:
+            return
+        if self.header.right == self.header:
+            if self.current_coverage > self.best_coverage:
+                self.best_coverage = self.current_coverage
+                self.best_solution = self.current_solution.copy()
+            return
+        column = self.choose_column()
+        if column.size == 0:
+            if self.current_coverage > self.best_coverage:
+                self.best_coverage = self.current_coverage
+                self.best_solution = self.current_solution.copy()
+                return
+        self.cover(column)
+        col_pointer = column.down
+        while col_pointer != column:
+            self.current_solution.append(col_pointer.row_id)
+            self.current_coverage += self.length_row(col_pointer)
+            row_pointer = col_pointer.right
+            while row_pointer != col_pointer:
+                self.cover(row_pointer.column)
+                row_pointer = row_pointer.right
+            self.search_uf_cover()
+            self.current_solution.pop()
+            self.current_coverage -= self.length_row(col_pointer)
+            row_reverse_pointer = col_pointer.left
+            while row_reverse_pointer != col_pointer:
+                self.uncover(row_reverse_pointer.column)
+                row_reverse_pointer = row_reverse_pointer.left
+        self.uncover(column)
+        return
+
+    ### Max Coverage Underfit Algorithm ###
+
+    def search_uf_density(self): # This algorithms finds the underfit (no overlap) that covers with the highest number of rows (max density)
+        if (self.feasible_count//self.min_row_length) + self.current_density <= self.best_density:
+            return
+        if self.header.right == self.header:
+            if self.current_density > self.best_density:
+                self.best_density = self.current_density
+                self.best_solution = self.current_solution.copy()
+            return
+        column = self.choose_column()
+        if column.size == 0:
+            if self.current_density > self.best_density:
+                self.best_density = self.current_density
+                self.best_solution = self.current_solution.copy()
+            return
+        self.cover(column)
+        col_pointer = column.down
+        while col_pointer != column:
+            self.current_solution.append(col_pointer.row_id)
+            self.current_density += 1
+            row_pointer = col_pointer.right
+            while row_pointer != col_pointer:
+                self.cover(row_pointer.column)
+                row_pointer = row_pointer.right
+            self.search_uf_density()
+            self.current_solution.pop()
+            self.current_density -= 1
+            row_reverse_pointer = col_pointer.left
+            while row_reverse_pointer != col_pointer:
+                self.uncover(row_reverse_pointer.column)
+                row_reverse_pointer = row_reverse_pointer.left
+        self.uncover(column)
+        return
